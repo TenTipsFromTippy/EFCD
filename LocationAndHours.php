30b90d170ec2841cd2694920710813bd57dd5b53
TippyT <tippy6227@mail.philau.edu>
2016-12-15 14:24:59 -0500

    Initial Upload

---
+++ LocationAndHours.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  
+    <title>Location and Hours</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/mystyle.css" rel="stylesheet">
+    <link href="css/bootstrap_datepicker_standalone.less.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap_datepicker.css" rel="stylesheet" type="text/css">
+
+	</head>
+<body>
+<?php include 'nav2.php' ?>
+	<!-- <h1>Does it work?</h1> -->
+
+<div class="display_container">
+	
+	<div class ="row">
+		<div class= "col-md-8 col-md-offset-2 col-xs-12">
+			<div class= "heading">
+						<H3>Hours</H3>
+						<br>
+						<p>Monday through Saturday: 9am to 6pm</p>
+						<p>Sundays: Closed</p>
+
+						<H3>Location</H3>
+						<p>Come visit us today at 3500 Midvale Avenue</p>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3055.7435265608506!2d-75.1940910849638!3d40.01417608794782!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6b8652b0d8105%3A0xa513399ecad0eefe!2s3500+Midvale+Ave%2C+Philadelphia%2C+PA+19129!5e0!3m2!1sen!2sus!4v1481824499457" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+		</div>
+	</div>
+</div>
+
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> -->
+
+
+	</body>
+</html>
